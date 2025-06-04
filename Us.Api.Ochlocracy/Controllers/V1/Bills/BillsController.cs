711b7e8e214e3ffdb2731366dd85838cbaa81779
Michael Cuomo <mcuomo777@gmail.com>
2025-06-03 22:05:00 -0400

    Add most of the other Bills Endpoint and Stub out the Details Endpoint

--- Us.Api.Ochlocracy/Controllers/V1/Bills/BillsController.cs
+++ Us.Api.Ochlocracy/Controllers/V1/Bills/BillsController.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using Us.Api.Ochlocracy.Abstractions;
+using Us.Ochlocracy.Model;
 using Us.Ochlocracy.Model.Api;
 using Us.Ochlocracy.Model.Bills;
 using Us.Ochlocracy.Service.V1;
@@ -41,4 +42,56 @@ public class BillsController(IMediator mediator) : RespondController
         [FromQuery] int offset = 0,
         [FromQuery] int limit = 10) =>
         Respond(await mediator.Send(new GetPagedBills(offset, limit)));
+    
+    /// <summary>
+    /// Gets a paged response of bills of a specified congress.
+    /// </summary>
+    /// <param name="congressId">The number used to identify the congress.</param>
+    /// <param name="offset">The offset used for paging the bills.</param>
+    /// <param name="limit">The number of bills to return.</param>
+    /// <returns></returns>
+    [HttpGet("{congressId:int}")]
+    [SwaggerOperation("Retrieves a paged response of the bills of a given congress.")]
+    [ProducesResponseType(Status200OK, Type = typeof(BillPartialResponse))]
+    public async Task<IActionResult> RetrieveBillsByCongressId(
+        [FromRoute] int congressId,
+        [FromQuery] int offset = 0,
+        [FromQuery] int limit = 10) =>
+        Respond(await mediator.Send(new GetCongressPagedBills(congressId, offset, limit)));
+
+    /// <summary>
+    /// Gets a paged response of bills of a specified congress, filtered by bill type.
+    /// </summary>
+    /// <param name="congressId">The number used to identify the congress.</param>
+    /// <param name="billType">The type of bill to retrieve.</param>
+    /// <param name="offset">The offset used for paging the bills.</param>
+    /// <param name="limit">The number of bills to return.</param>
+    /// <returns></returns>
+    [HttpGet("{congressId}/{billType}")]
+    [SwaggerOperation("Retrieves a paged response of the bills of a given congress, filtered by bill type.")]
+    [ProducesResponseType(Status200OK, Type = typeof(BillPartialResponse))]
+    public async Task<IActionResult> RetrieveBillsByCongressIdAndBillType(
+        [FromRoute] int congressId,
+        [FromRoute] BillType billType,
+        [FromQuery] int offset = 0,
+        [FromQuery] int limit = 10) =>
+        Respond(await mediator.Send(new GetCongressPagedBillsByType(congressId, billType, offset, limit)));
+
+    // TODO: Does not work currently, the model for the object being returned is incorrect
+    /// <summary>
+    /// Retrieves the bill detail using the congress number, bill type, and bill number.
+    /// </summary>
+    /// <param name="congressId">The identifier used to specify the congress.</param>
+    /// <param name="billType">The type of bill to retrieve.</param>
+    /// <param name="billNumber">The number used to identify the specific bill.</param>
+    /// <returns></returns>
+//     [HttpGet("{congressId}/{billType}/{billNumber}")]
+//     [SwaggerOperation(
+//         "Retrieves the bill detail of the specified bill using the congress, bill type, and bill number.")]
+//     [ProducesResponseType(Status200OK, Type = typeof(BillResponse))]
+//     public async Task<IActionResult> RetrieveBillDetail(
+//         [FromRoute] int congressId,
+//         [FromRoute] BillType billType,
+//         [FromRoute] string billNumber) =>
+//         Respond(await mediator.Send(new GetBillDetail(congressId, billType, billNumber)));
 }
--- Us.Ochlocracy.Service/V1/Validators/BillValidators.cs
+++ Us.Ochlocracy.Service/V1/Validators/BillValidators.cs
@@ -12,3 +12,34 @@ public class GetPagedBillsValidator : AbstractValidator<GetPagedBills>
         RuleFor(x => x.Offset).GreaterThanOrEqualTo(0).WithMessage("Offset must be a positive number.");
     }
 }
+
+public class GetCongressPagedBillsValidator : AbstractValidator<GetCongressPagedBills>
+{
+    public GetCongressPagedBillsValidator()
+    {
+        RuleFor(x => x.Limit).GreaterThan(0).WithMessage("Limit must be greater than 0.");
+        RuleFor(x => x.Limit).LessThanOrEqualTo(100).WithMessage("Limit must be less than or equal to 100.");
+        RuleFor(x => x.Offset).GreaterThanOrEqualTo(0).WithMessage("Offset must be a positive number.");
+        RuleFor(x => x.Congress).GreaterThan(0).WithMessage("Congress Number must be greater than 0.");
+    }
+}
+
+public class GetCongressPagedBillByTypeValidator : AbstractValidator<GetCongressPagedBillsByType>
+{
+    public GetCongressPagedBillByTypeValidator()
+    {
+        RuleFor(x => x.Limit).GreaterThan(0).WithMessage("Limit must be greater than 0.");
+        RuleFor(x => x.Limit).LessThanOrEqualTo(100).WithMessage("Limit must be less than or equal to 100.");
+        RuleFor(x => x.Offset).GreaterThanOrEqualTo(0).WithMessage("Offset must be a positive number.");
+        RuleFor(x => x.Congress).GreaterThan(0).WithMessage("Congress Number must be greater than 0.");
+    } 
+}
+
+public class GetBillDetailValidator : AbstractValidator<GetBillDetail>
+{
+    public GetBillDetailValidator()
+    {
+        RuleFor(x => x.Congress).GreaterThan(0).WithMessage("Congress Number must be greater than 0.");
+        RuleFor(x => x.BillNumber).NotEmpty().WithMessage("Bill number must not be empty.");
+    }
+}
